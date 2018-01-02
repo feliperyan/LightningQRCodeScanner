42b768f3df3c0377126a71ee48b8e33251dc1d4a
Felipe Ryan <fryan@fryan-ltm22.internal.salesforce.com>
2018-01-02 18:53:29 +1100

    first commit

---
+++ force-app/main/default/aura/QRCodeVersionTwoComponent/QRCodeVersionTwoComponentController.js
@@ -0,0 +1,44 @@
+({
+    scriptsLoaded : function(component, event, helper) {
+        console.log('Scripts loaded.');
+        
+        _qrcode.callback = function(data) {
+            //console.log(data);
+            //helper.helperMethod(component, null, data);
+            
+            var appEvent = $A.get("e.c:CodeScanned");
+            appEvent.setParams({'data': data});
+            appEvent.fire();
+        }
+    },
+
+    handleCodeScannedEvent : function(cmp, event, helper) {
+        console.log('Capture Code Scanned Event');
+        var eventData = event.getParam('data');
+        helper.helperMethod(cmp, null, eventData);
+    },
+    
+    previewFile : function(cmp, event, helper) {
+        var the_image = new Image();
+        var the_canvas = cmp.find('qr-canvas').getElement().getContext('2d');
+        var file =       cmp.find('the_file').getElement().files[0];
+        var reader =     new FileReader();
+
+        reader.addEventListener("load", function () {
+            the_image.src = reader.result;
+            _qrcode.decode(reader.result);
+
+        }, false);
+
+        the_image.onload = function(){
+            console.log('Drawing');
+            the_canvas.canvas.height = the_image.height;
+            the_canvas.canvas.width = the_image.width;
+            the_canvas.drawImage(the_image, 0, 0, the_canvas.canvas.width/5, the_canvas.canvas.height/5);                
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+})
---
+++ force-app/main/default/aura/QRCodeVersionTwoComponent/QRCodeVersionTwoComponentHelper.js
@@ -0,0 +1,6 @@
+({
+    helperMethod : function(cmp, obj, data) {
+        cmp.set('v.status', data)
+        console.log(data);        
+    }
+})
