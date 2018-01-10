bef991d619b16d5706597c1467a4e66b57313971
Jiun Ryu <jryu@salesforce.com>
2018-01-10 14:20:38 +1100

    Jiun's Changes

--- force-app/main/default/aura/QRCodeVersionTwoComponent/QRCodeVersionTwoComponentController.js
+++ force-app/main/default/aura/QRCodeVersionTwoComponent/QRCodeVersionTwoComponentController.js
@@ -18,27 +18,34 @@
         helper.helperMethod(cmp, null, eventData);
     },
     
-    previewFile : function(cmp, event, helper) {
+    previewFile : function(cmp, event, helper) {        
         var the_image = new Image();
         var the_canvas = cmp.find('qr-canvas').getElement().getContext('2d');
-        var file =       cmp.find('the_file').getElement().files[0];
+        var file =       event.getSource().get('v.files')[0];
         var reader =     new FileReader();
 
         reader.addEventListener("load", function () {
             the_image.src = reader.result;
             _qrcode.decode(reader.result);
-
         }, false);
 
         the_image.onload = function(){
             console.log('Drawing');
-            the_canvas.canvas.height = the_image.height;
-            the_canvas.canvas.width = the_image.width;
-            the_canvas.drawImage(the_image, 0, 0, the_canvas.canvas.width/5, the_canvas.canvas.height/5);                
+            //the_canvas.canvas.height = the_image.height;
+            //the_canvas.canvas.width = the_image.width;
+            the_canvas.canvas.height = 300;
+            the_canvas.canvas.width  = 300;
+            the_canvas.drawImage(the_image, 0, 0, the_canvas.canvas.width, the_canvas.canvas.height);
+            var canvas = cmp.find('qr-canvas');
+            $A.util.removeClass(canvas, 'slds-hide');
         };
 
         if (file) {
             reader.readAsDataURL(file);
         }
+    },
+    
+    toggleSpinner : function(cmp, event, helper) {
+        helper.toggleSpinner(cmp);
     }
 })
--- force-app/main/default/aura/QRCodeVersionTwoComponent/QRCodeVersionTwoComponentHelper.js
+++ force-app/main/default/aura/QRCodeVersionTwoComponent/QRCodeVersionTwoComponentHelper.js
@@ -2,5 +2,9 @@
     helperMethod : function(cmp, obj, data) {
         cmp.set('v.status', data)
         console.log(data);        
+    },
+    toggleSpinner : function(cmp) {
+        var spinner = cmp.find('mySpinner');
+        $A.util.toggleClass(spinner, 'slds-hide');
     }
 })
